e1ee7076eddebe2363a10045e109af4e5df13513
karakiri-kun <fahrezapurnomo162@gmail.com>
2025-05-22 10:44:42 +0700

    define struct dan enumeration

--- main.c
+++ main.c
@@ -1,4 +1,27 @@
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_SEKTOR 10
+
+typedef enum {
+    ANGKOT,
+    BUS
+} JenisKendaraan;
+
+typedef enum {
+    LISTRIK,
+    DIESEL
+} TipeKendaraan;
+
+typedef struct {
+    char nama[50];
+    float jarak;
+    char jalan_raya;
+    int demand;
+    JenisKendaraan jenis;
+    TipeKendaraan tipe;
+    int kendaraan_dialokasikan;
+} Sektor;
 
 int main() {
 	
